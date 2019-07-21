8b0ba9403daaf1b2f2f0c0b40339066ec2743beb
ramshakhan123 <49521382+ramshakhan123@users.noreply.github.com>
2019-07-21 18:42:22 +0500

    added files...

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let paraIntelligenceValue = document.querySelector("#see-the-intelligence");
-let userName = document.querySelector("input");
+let intelligenceName = ["Talented",
+    "Multi-talented",
+    "Superb",
+    "Einstein",
+    "Dumb",
+    "Intelligent", "Smart", "Good Person", "Great",
+    "Mastermind", "Hard working", "Average", "Extra ordinary", "Brave"]
 
 function check() {
-    let intelligenceOfAPerson = ["Good Person", "Multi Talented"];
-    paraIntelligenceValue.innerHTML = userName.value;
-    paraIntelligenceValue.innerHTML = userName.value + intelligenceOfAPerson;
+    let userInputName = document.querySelector("input").value;
+    let randomIndex = parseInt(Math.random() * intelligenceName.length);
+    document.querySelector("p").innerHTML = intelligenceName[randomIndex];
 }
-
-
-
